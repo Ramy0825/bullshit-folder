9c777c84fe482b6fa661f5fa8329a3ba3391351e
Ramy <ramy3@usf.edu>
2025-01-23 12:59:24 -0500

    sdfghjkl

---
+++ cc_1.js
@@ -0,0 +1,15 @@
+// Task 1: Variables
+let employeeName = "Ramy Abdelbaky";
+const employeeID = 4444;
+var isActive = true;
+
+console.log ("EmployeeName:", employeeName, "- type:", typeof employeeName);
+console.log ("EmployeeID:", employeeID, "- type:", typeof employeeID)
+console.log ("Is Active:", isActive, "- type:", typeof isActive)
+
+// Task 2: Primitive Data Types 
+let productName = "Beanie";
+const productPrice = 25;
+var  isAvailable = true;
+
+
---
+++ script.js
@@ -0,0 +1,6 @@
+//alert("AAAAHHHH THE BEES!!!")
+
+console.log("Hello! I'm in the console!")
+
+
+console.log("Hello, I'm bob")
